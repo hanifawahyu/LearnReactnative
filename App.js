f171e57cc7c215ba14ba496bdc981b3e498256b2
Hanifa Wahyu <hanifawahyuu@gmail.com>
2018-10-09 20:39:49 +0700

    reactnavigation

--- App.js
+++ App.js
@@ -1,55 +1,33 @@
-
-          /**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View,Image} from 'react-native';
-import Judul from './Component/Judul';
+import {Platform, StyleSheet, Text, View, Image} from 'react-native';
+import { createStackNavigator } from 'react-navigation';
+import { StackNavigator } from 'react-navigation';
 
-type Props = {};
+
+import Judul from './Component/Judul';
+import Login from './Component/Login';
+
+const AppNavigator = createStackNavigator ({
+       Login : Login,
+       Judul : Judul,
+})
+
+
 export default class App extends Component<Props> {
   render() {
     return (
-      <View style={styles.container}>
-      <Judul judul = "BIODATA"/>
-      <Judul judul = "LOGIN"/>
-      <Judul judul = "FORM"/>
-      <View>
-        <Text style={styles.welcome}>Hellooooo!</Text>        
-        <Text style={styles.instructions}>Nama   : Hanifa Wahyu Listiyani</Text>
-        <Text style={styles.instructions}>No        :  15</Text>
-        <Text style={styles.instructions}>Kelas   : XI RPL 1</Text>
-        <Image style = {{ width : 300, height : 300 }}
-        source={require('./tataifa.jpg')}
-        />     
-      </View>
-      </View>
+
+      <AppNavigator/>
+
     );
   }
-}
+} 
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex: 1,   
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor : 'black'
-  },
-  welcome: {
-    fontSize: 25,
-    textAlign: 'center',
-    margin: 10,
-    color: 'white',
-  },
-  instructions: {
-    fontSize : 18,
-    textAlign: 'left',
-    color: 'white',
-    marginBottom: 5,
-  },
-});
+    backgroundColor: '#ffff',
+  },  
+ });
--- Component/Judul.js
+++ Component/Judul.js
@@ -1,19 +1,49 @@
 import React, {Component} from 'react';
-import {Text} from 'react-native';
+import {View,Text,StyleSheet,Button,Alert,Image} from 'react-native';
 
-class Judul extends Component{
+export default class Judul extends Component{
+	redirect()
+	{
+			Alert.alert = ("This Button is ready")
+			
+	}
+
 	render(){
-		return (
-			<Text style = {hani.judul}>{this.props.judul}</Text>
+		return(
+			<View style = {styles.container}>
+			<Image source ={require('./batang.jpg')} style={{width: 200, height: 200}}/> 
+			<Text style = {warna.ifa}> Selamat datang Ifa !! </Text>
+			<Text style = {salon.hani} > Nama : Hanifa Wahyu Listiyani</Text>
+			<Text style = {salon.hani} > Kelas : XI RPL 1 </Text>
+			<Text style = {salon.hani} > SMK TELKOM Purwokerto</Text>
+			   <Text style = {{ marginBottom : 100}}></Text>
+			   <Button title = " Next " color = "#68098F" onPress = {() => {this.redirect()}}/>
+			</View>
+
+
 			)
 	}
 }
-const hani = {
-	judul : {
-		color : 'black',
-		fontSize : 50,
+
+const styles = StyleSheet.create({
+	container : {
+	flex : 1,
+    justifyContent : 'center',
+    alignItems : 'center',
+    backgroundColor : '#F3D5FF',
+	}
+});
+const warna = {
+	ifa  : {
+		color : "#5B087E",
+		fontSize : 20,
 		fontWeight : 'bold',
-		backgroundColor : 'white',
 	}
 }
-export default Judul;
+
+const salon = {
+	hani : {
+		color : "#8B14BC",
+		fontSize : 20,
+	}
+}
